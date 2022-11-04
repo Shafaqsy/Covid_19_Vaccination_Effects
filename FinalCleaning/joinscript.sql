677bcb42510c15102225ed4f8504f9af019bcd67
ferminb10 <107658895+ferminb10@users.noreply.github.com>
2022-11-03 19:33:59 -0700

    Delete joinscript.sql

--- FinalCleaning/joinscript.sql
+++
@@ -1,4 +0,0 @@
-select covid_data.date, covid_data.cases, covid_data.deaths, vaccination_data.total_doses, vaccination_data.cumulative_total_doses
-from covid_data
-inner join vaccination_data
-on covid_data.date = vaccination_data.date;
