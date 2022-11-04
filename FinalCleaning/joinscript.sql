cdec6cf1e2f3fa83fdf001e2e3089b441305715e
ferminb10 <107658895+ferminb10@users.noreply.github.com>
2022-11-03 19:34:38 -0700

    Add files via upload

---
+++ FinalCleaning/joinscript.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Masterfile AS
+select covid_data.date, covid_data.cases, covid_data.deaths, vaccination_data.total_doses, vaccination_data.cumulative_total_doses
+from covid_data
+inner join vaccination_data
+on covid_data.date = vaccination_data.date;
